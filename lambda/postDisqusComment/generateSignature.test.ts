e14837e9e90bedb436e4b30893f17f02efda4e92
Jordan Allen <jordan@jordanallen.org>
2022-11-16 14:14:19 +0000

    Throw an error if DISQUS_SECRET is not present
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- lambda/postDisqusComment/generateSignature.test.ts
+++ lambda/postDisqusComment/generateSignature.test.ts
@@ -10,17 +10,27 @@ describe("generateSignature", () => {
     jest.useRealTimers();
   });
 
-  it("generates a valid Disqus SSO signature", () => {
-    const user: DisqusData = {
-      id: "3380ec0f-cae9-4927-b8d4-fc5d0fa012fd",
-      username: "Test User",
-      email: "test@test.com",
-    };
+  const user: DisqusData = {
+    id: "3380ec0f-cae9-4927-b8d4-fc5d0fa012fd",
+    username: "Test User",
+    email: "test@test.com",
+  };
 
-    const signature = generateSignature(user, "SECRET_KEY");
+  it("generates a valid Disqus SSO signature", () => {
+    process.env.DISQUS_SECRET = "SECRET_KEY";
+
+    const signature = generateSignature(user);
 
     expect(signature).toBe(
       "eyJpZCI6IjMzODBlYzBmLWNhZTktNDkyNy1iOGQ0LWZjNWQwZmEwMTJmZCIsInVzZXJuYW1lIjoiVGVzdCBVc2VyIiwiZW1haWwiOiJ0ZXN0QHRlc3QuY29tIn0= 1df49f9f32fcd4bbba7d58684cf586d2c10f842b 1585695600"
+    );
+  });
+
+  it("throws an error if the DISQUS_SECRET variable is missing", () => {
+    process.env.DISQUS_SECRET = "";
+
+    expect(() => generateSignature(user)).toThrowError(
+      "Missing environment variable: DISQUS_SECRET"
     );
   });
 });
--- lambda/postDisqusComment/generateSignature.ts
+++ lambda/postDisqusComment/generateSignature.ts
@@ -6,13 +6,16 @@ export interface DisqusData {
   email: string;
 }
 
-export const generateSignature = (data: DisqusData, DISQUS_SECRET: string) => {
+export const generateSignature = (data: DisqusData) => {
+  if (!process.env.DISQUS_SECRET)
+    throw new Error("Missing environment variable: DISQUS_SECRET");
+
   const disqusStr = JSON.stringify(data);
   const timestamp = Math.round(+new Date() / 1000);
 
   const message = Buffer.from(disqusStr).toString("base64");
 
-  const result = HmacSHA1(message + " " + timestamp, DISQUS_SECRET);
+  const result = HmacSHA1(message + " " + timestamp, process.env.DISQUS_SECRET);
   const hexsig = enc.Hex.stringify(result);
 
   return message + " " + hexsig + " " + timestamp;
